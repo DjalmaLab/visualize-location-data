d9d01a6d98fab59ee64e709874d7be5a1e3a49d8
Chris Archibald <carchi8py@gmail.com>
2016-04-18 17:43:14 -0700

    Created a list of my google locations

---
+++ locationObj.py
@@ -0,0 +1,6 @@
+class LocationObj(object):
+    def __init__(self, time, lat, lon, altitude):
+        self.time = time
+        self.lat = lat
+        self.lon = lon
+        self.altitude = altitude
--- visualize.py
+++ visualize.py
@@ -1,7 +1,19 @@
 import json
 from data import google_location_data
+from locationObj import LocationObj
+import sys
 
+myLocations = []
 with open(google_location_data) as dataFile:
     data = json.load(dataFile)
     locations = data["locations"]
-    print len(locations)
+    for location in locations:
+        try:
+            location["altitude"]
+        except:
+            location["altitude"] = 0
+        myLocations.append(LocationObj(location["timestampMs"],
+                                        location['latitudeE7'],
+                                        location["longitudeE7"],
+                                        location["altitude"]))
+print len(myLocations)
